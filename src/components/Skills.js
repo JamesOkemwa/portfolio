679e0461690135c8ea721a5b4e8e32b54551e45f
lejames <okejames55@gmail.com>
2023-02-18 23:53:05 +0300

    Add Skills section assests

---
+++ src/components/Skills.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+import JavaScript from '../assets/javascript.png'
+import ReactImg from '../assets/react.png'
+import Tailwind from '../assets/tailwind.png'
+
+const Skills = () => {
+  return (
+    <div>Skills</div>
+  )
+}
+
+export default Skills
