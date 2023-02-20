898aec231851a41609fc35cb0d7c872c8a0f0ba6
lejames <okejames55@gmail.com>
2023-02-20 12:11:03 +0300

    Add Github and Demo link buttons to project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -18,6 +18,14 @@ const ProjectCard = ({ project }) => {
                 )
             })}
         </div>
+        <div className="flex justify-between px-6 pt-4 pb-2">
+            <a href={project.githubLink}>
+                <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-4 py-2 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Github</button>
+            </a>
+            <a href={project.demoLink}>
+                <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-4 py-2 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Demo</button>
+            </a>
+        </div>
     </div>
   )
 }
